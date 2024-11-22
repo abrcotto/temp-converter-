3eca779f8cc56945033b639d7f225e431f888c62
abrcotto <acotto@mocs.flsouthern.edu>
2024-11-22 10:25:20 -0500

    Hw Finished

--- projetct 1.py
+++ projetct 1.py
@@ -2,19 +2,20 @@ import tkinter as tk
 window = tk.Tk()
 
 fahren_var = tk.StringVar()
+Lbl_celsius = tk.StringVar()
 
 def Temp_fahren():
     fahren = int(fahren_var.get())
     celsius = (fahren - 32) * 5/9
-    Lbl_celsius = celsius
+    Lbl_celsius['text'] = celsius
     fahren_var.set("")
+
 
 Entry_temp = tk.Entry(textvariable=fahren_var)
 Lbl_temp = tk.Label(text='°F')
-temp_button = tk.Button(text='------>')
+temp_button = tk.Button(text='------>',command=Temp_fahren)
 Lbl_celsius = tk.Label()
 
-#ent_password.pack()
 Entry_temp.grid(row=0,column=1)
 Lbl_temp.grid(row=0,column=2)
 temp_button.grid(row=0,column=3)
